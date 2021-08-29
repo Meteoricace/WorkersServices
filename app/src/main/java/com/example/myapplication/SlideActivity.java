3cf3c2ff8eee29597727de483bbc931b63850f3d
KRATOS <Meteoricace@gmail.com>
2021-08-29 19:10:33 +0200

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/SlideActivity.java
@@ -0,0 +1,23 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SlideActivity extends AppCompatActivity {
+    public static ViewPager viewPager;
+    SlideViewPagerAdapter adapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_slide);
+        viewPager = findViewById(R.id.viewpager);
+        adapter = new SlideViewPagerAdapter(this);
+        viewPager.setAdapter(adapter);
+        getSupportActionBar().hide();
+    }
+
+}
---
+++ app/src/main/java/com/example/myapplication/splash_screen.java
@@ -0,0 +1,29 @@
+package com.example.myapplication;
+
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class splash_screen extends AppCompatActivity {
+    Handler mHandler;
+    Runnable mRunnable;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+        mHandler=new Handler();
+        mRunnable= new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(splash_screen.this,SlideActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        };
+        mHandler.postDelayed(mRunnable,3000);
+        getSupportActionBar().hide();
+
+    }
+}
