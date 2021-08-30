06a7a222c2e8959c1315567d42531ef5306d075c
KRATOS <Meteoricace@gmail.com>
2021-08-30 20:01:48 +0200

    int

--- app/src/main/java/com/example/myapplication/SlideActivity.java
+++ app/src/main/java/com/example/myapplication/SlideActivity.java
@@ -14,10 +14,28 @@ public class SlideActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_slide);
-        viewPager = findViewById(R.id.viewpager);
-        adapter = new SlideViewPagerAdapter(this);
+        viewPager=findViewById(R.id.viewpager);
+        adapter= new SlideViewPagerAdapter(this);
         viewPager.setAdapter(adapter);
         getSupportActionBar().hide();
+        if(sOpenedAlready())
+        {
+            Intent intent=new Intent(SlideActivity.this,MainActivity.class);
+            intent.setFlags(intent.FLAG_ACTIVITY_CLEAR_TASK| Intent.FLAG_ACTIVITY_NEW_TASK);
+            startActivity(intent);
+
+        }
+        else{
+            SharedPreferences.Editor editor=getSharedPreferences("slide",MODE_PRIVATE).edit();
+            editor.putBoolean("slide",true);
+            editor.commit();
+
+        }
     }
 
+    private boolean sOpenedAlready() {
+        SharedPreferences sharedPreferences =getSharedPreferences("slide",MODE_PRIVATE);
+        boolean result = sharedPreferences.getBoolean("slide",false);
+        return result;
+    }
 }
