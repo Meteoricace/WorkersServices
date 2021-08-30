e2de39963d43bd3039f0680b9b6c71e216ce1867
KRATOS <Meteoricace@gmail.com>
2021-08-30 22:54:20 +0200

    2nd

--- app/src/main/java/com/example/myapplication/Electrical.java
+++ app/src/main/java/com/example/myapplication/Electrical.java
@@ -1,13 +1,55 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Electrical extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+    private Button button3;
+    private Button button4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_electrical1);
+        button1 = findViewById(R.id.electrical_button_electrical);
+        button2 = findViewById(R.id.electrical_button_plumbing);
+        button3 = findViewById(R.id.electrical_button_cleaning);
+        button4 = findViewById(R.id.electrical_button_other);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openServices();
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openServices();
+
+            }
+        });
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openServices();
+
+            }
+        });
+        button4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openServices();
+
+            }
+        });
     }
+    public void openServices() {
+        Intent intent = new Intent(this, services.class);
+        startActivity(intent);
+    }
+
+
 }
--- app/src/main/java/com/example/myapplication/carpentry1.java
+++ app/src/main/java/com/example/myapplication/carpentry1.java
@@ -1,13 +1,40 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class carpentry1 extends AppCompatActivity {
+    private Button button1;
+    private Button button2;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_carpentry1);
+        button1 = findViewById(R.id.carpentry_button_fabrication);
+        button2 = findViewById(R.id.carpentry_button_other);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openServices();
+
+            }
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {openServices();
+
+            }
+        });
     }
+    public void openServices() {
+        Intent intent = new Intent(this, services.class);
+        startActivity(intent);
+    }
+
+
 }
